7f989e607589c5bfa4bc5b2fa560baa179403512
miffii <iqamar590@gmail.com>
2021-06-14 00:01:49 +0500

    login and sidebar

---
+++ public/resources/js/custom-admin.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    $("#close-sidebar").click(function() {
+        if ($(".page-wrapper").hasClass("toggled")){
+            $(".page-wrapper").removeClass("toggled");
+        }else
+        {
+            $(".page-wrapper").addClass("toggled");
+        }
+    });
+});
