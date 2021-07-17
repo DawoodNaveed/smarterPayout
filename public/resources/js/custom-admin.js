167ad911dc0d34c1ca00f846880e35939db42ca7
iqamar590@gmail.com <iqamar590@gmail.com>
2021-07-17 18:40:51 +0500

    leads interaction

--- public/resources/js/custom-admin.js
+++ public/resources/js/custom-admin.js
@@ -1,19 +1,33 @@
 $(document).ready(function () {
-
     $("#close-sidebar").click(function () {
-        if ($(".page-wrapper").hasClass("toggled")) {
-            $(".page-wrapper").removeClass("toggled");
+        var pageWrapper = '.page-wrapper';
+        if ($(pageWrapper).hasClass("toggled")) {
+            $(pageWrapper).removeClass("toggled");
         } else {
-            $(".page-wrapper").addClass("toggled");
+            $(pageWrapper).addClass("toggled");
         }
     });
-
+    $("#close-right-sidebar").click(function () {
+        var sidebarWrapperRight = '.sidebar-wrapper-right';
+        if ($(sidebarWrapperRight).hasClass("collapsed")) {
+            $(this).removeClass("animate-open");
+            $(this).addClass("animate-close");
+            $(sidebarWrapperRight).removeClass("collapsed");
+            $(sidebarWrapperRight).addClass("open-right-sidebar ");
+            $(sidebarWrapperRight).find('i').removeClass("fa-angle-double-left");
+            $(sidebarWrapperRight).find('i').addClass("fa-angle-double-right");
+        } else {
+            $(this).removeClass("animate-close");
+            $(this).addClass("animate-open");
+            $(sidebarWrapperRight).addClass("collapsed");
+            $(sidebarWrapperRight).removeClass("open-right-sidebar ");
+            $(sidebarWrapperRight).find('i').removeClass("fa-angle-double-right");
+            $(sidebarWrapperRight).find('i').addClass("fa-angle-double-left");
+        }
+    });
     $('#data_table').DataTable();
-
-    $('.deleteRecord').click(function (){
-        let recordUrl=$(this).data('url');
-        console.log(recordUrl);
+    $('.deleteRecord').click(function () {
+        let recordUrl = $(this).data('url');
         $("#deletebtn").attr("href", recordUrl);
-
     });
 });
