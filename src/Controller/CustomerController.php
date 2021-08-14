9ff760fa3b12da87ec01a0401a9413457e644222
Dawood <dawood.naveed@coeus-solutions.de>
2021-08-14 22:18:59 +0500

    remove extra code

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -19,46 +19,6 @@ use Symfony\Component\Routing\Annotation\Route;
 class CustomerController extends AbstractController
 {
     /**
-     * @Route("/createCustomer", name="create_customer")
-     * @param Request $request
-     */
-    public function insertCustomer(CustomerRepository $userRepository, Request $request)
-    {
-        #TODO
-        $customer = new Customer();
-        $customerMeta = new CustomerMeta();
-        $customer->setFirstName('iffi');
-        $customer->setLastName('iffi2');
-        $customer->setDateOfBirth(new \DateTime());
-        $customer->setAge(28);
-        $customer->setGender(1);
-        $customer->setHeight(16);
-        $customer->setWeight(20);
-        $customerMeta->setAnnualCheckupStatus(1);
-        $customerMeta->setBloodPressureStatus(1);
-        $customerMeta->setDrivingInfraction(1);
-        $customerMeta->setDuiStatus(1);
-        $customerMeta->setHealthStatus(1);
-        $customerMeta->setLegalIssueStatus(1);
-        $customerMeta->setMisdemeanorStatus(1);
-        $customerMeta->setPaymentStartDate(new \DateTime());
-        $customerMeta->setPaymentEndDate(new \DateTime());
-        $customerMeta->setPercentStep(2);
-        $customerMeta->setPaymentFrequency(1);
-        $customerMeta->setPaymentType('2');
-        $customerMeta->setSmoker(1);
-        $customerMeta->setPhysicalExerciseStatus(1);
-        $customerMeta->setHighCholesterol(2);
-        $customerMeta->setPhoneNumber1(+921515);
-        $customerMeta->setPhoneNumber2(+921515);
-        $customerMeta->setPhoneNumber3(+921515);
-        $customerMeta->setCustomer($customer);
-        $userRepository->addEditUser($customer, $customerMeta);
-        dump('added');
-        die();
-    }
-    
-    /**
      * @Route("/customers", name="get_customers", methods={"GET"})
      * @return Response
      */
