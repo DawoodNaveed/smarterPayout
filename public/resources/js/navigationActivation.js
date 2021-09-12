36f6f1c2b3483d15fcf72e35c2b00b4d9c8e8b9a
iffi <iqamar590@gmail.com>
2021-09-12 10:18:42 +0000

    job details and about us details pages finalized

--- public/resources/js/navigationActivation.js
+++ public/resources/js/navigationActivation.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
-    //  in case of page scroll
-    $(window).scroll(function() {
+$(document).ready(function () {
+//  in case of page scroll
+    $(window).scroll(function () {
         var distance = $(window).scrollTop();
-        $('.page-section').each(function(i) {
-            if($(this).position().top <= distance + 150) {
+        $('.page-section').each(function (i) {
+            if ($(this).position().top <= distance + 150) {
                 $('.navbar-nav .nav-item.active').removeClass('active');
                 $('.navbar-nav .nav-item').eq(i).addClass('active');
             }
@@ -12,10 +12,16 @@ $(document).ready(function() {
 
     //  in case of navigation link click
     var navigationLinks = $(".navbar .navbar-collapse .navbar-nav .nav-item");
-    for(var i = 0; i < navigationLinks.length; i++) {
-        navigationLinks[i].addEventListener("click", function() {
+    for (var i = 0; i < navigationLinks.length; i++) {
+        navigationLinks[i].addEventListener("click", function () {
             var current = document.getElementsByClassName("active");
-            current[0].className.replace(" active", "");
+            console.log(current);
+            if (typeof current[0] != "undefined") {
+                current[0].className.replace(" active", "");
+            }
+            var origin = window.location.origin;
+            console.log(origin + '/#' + $(this).find('a').data('url'));
+            window.location = origin + '/#' + $(this).find('a').data('url');
             this.className += " active";
         });
     }
