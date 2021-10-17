245b935a7ca2a88e98373d99831d307dc69d6ffd
iffi <iqamar590@gmail.com>
2021-10-17 13:51:41 +0000

    bug report 2 all fixed

--- public/resources/js/custom-client.js
+++ public/resources/js/custom-client.js
@@ -1,41 +1,36 @@
-$(document).ready(function () {
+$(document).ready(function() {
     // $("#contact_us_form_contact, #calculator_form_phoneNo, #inp-otp-phone").inputmask({"mask": "+1 (999) 999-9999"});
-
-    $(document).on('click', '.btn-spinner', function () {
+    $(document).on('click', '.btn-spinner', function() {
         var spinner = $(this).find('.fa-spinner');
-        if (spinner) {
+        if(spinner) {
             spinner.remove();
         }
-
         spinnerBtn = $(this);
         $(this).append(`<i class="fas fa-spinner fa-spin"></i>`);
-
         var form = $(this).parent('form');
-        if (form && !form.valid) {
+        if(form && !form.valid) {
             var spinner = spinnerBtn.find('.fa-spinner');
-            if (spinner) {
-                setTimeout(function () {
+            if(spinner) {
+                setTimeout(function() {
                     spinner.remove();
                 }, 300);
             }
         }
-
     });
-
     resize_carousel();
     // Set height of page sections according to screen size
     $('#about-us, #calculate, #career').css('min-height', $(window).height());
     // Smart scroll for navigation bar
-    if ($('.smart-scroll').length > 0 && $(window).width() >= 992) {
-        $(window).on('scroll', function () {
+    if($('.smart-scroll').length > 0 && $(window).width() >= 992) {
+        $(window).on('scroll', function() {
             scroll_top = $(this).scrollTop();
         });
     }
     //  handling navbar for mobile/tab screens
-    $('.navbar-toggler').click(function () {
+    $('.navbar-toggler').click(function() {
         var navbarTogglerIcon = $('.navbar-toggler span');
         var navbarCollapse = $('.navbar-collapse');
-        if (navbarCollapse.hasClass('show')) {
+        if(navbarCollapse.hasClass('show')) {
             navbarCollapse.toggleClass('show');
             navbarTogglerIcon.removeClass('fa fa-times');
             navbarTogglerIcon.addClass('fa fa-bars');
@@ -49,14 +44,14 @@ $(document).ready(function () {
         }
     });
     //  custom animated input fields
-    $("input, textarea, select").focus(function () {
+    $("input, textarea, select").focus(function() {
         $(this).parents(".form-group").addClass("focused");
         $(this).parents(".form-group").removeClass("has-error");
     });
-    $("input, textarea, select").on('blur keyup focusout change', function () {
-        if (!$(this).val() && !$(this).hasClass('static')) {
+    $("input, textarea, select").on('blur keyup focusout change', function() {
+        if(!$(this).val() && !$(this).hasClass('static')) {
             $(this).removeClass("filled");
-            if (!$(this).is(":focus")) {
+            if(!$(this).is(":focus")) {
                 $(this).parents(".form-group").removeClass("focused");
             }
         } else {
@@ -64,17 +59,6 @@ $(document).ready(function () {
             $(this).parents(".form-group").addClass("focused");
         }
     });
-
-    // //  custom animated input fields
-    // $("#calculator_form_phoneNo, #contact_us_form_contact, #inp-otp-phone").hover(function () {
-    //     $(this).parents(".form-group").addClass("focused");
-    //     $(this).parents(".form-group").removeClass("has-error");
-    // }, function () {
-    //     if (!$(this).focus()) {
-    //         $(this).parents(".form-group").removeClass("focused");
-    //     }
-    // });
-
     // about us image animation
     var $animation_elements = $('.animation-element');
     var $window = $(window);
@@ -83,12 +67,12 @@ $(document).ready(function () {
         var window_height = $window.height();
         var window_top_position = $window.scrollTop();
         var window_bottom_position = (window_top_position + window_height);
-        $.each($animation_elements, function () {
+        $.each($animation_elements, function() {
             var $element = $('.animation-element');
             var element_height = $element.outerHeight();
             var element_top_position = $element.offset().top;
             var element_bottom_position = (element_top_position + element_height);
-            if ((element_bottom_position >= window_top_position) && (element_top_position <= window_bottom_position)) {
+            if((element_bottom_position >= window_top_position) && (element_top_position <= window_bottom_position)) {
                 $element.removeClass('about-us-animated-img-block');
                 $element.addClass('about-us-animated-img-animate');
             } else {
@@ -102,7 +86,6 @@ $(document).ready(function () {
         // Set height of carousel image according to screen size
         $('.carousel-item img').css('height', $(window).height() - $('.navbar').outerHeight());
     }
-
     $window.on('scroll resize', check_if_in_view);
     $window.trigger('scroll');
     $window.on('resize', resize_carousel)
